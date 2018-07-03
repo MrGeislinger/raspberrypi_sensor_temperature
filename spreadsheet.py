7fdfb24531a05f82149b0fa596c160a0897aa16d
MrGeislinger <GitHub@MrGeislinger.com>
2018-07-02 19:53:37 -0700

    feat: Add Google API to sheet's scope

--- spreadsheet.py
+++ spreadsheet.py
@@ -5,12 +5,12 @@ from oauth2client.service_account import ServiceAccountCredentials
 sheet_name = ''    
  
 # use creds to create a client to interact with the Google Drive API
-scope = ['https://spreadsheets.google.com/feeds']
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
 creds = ServiceAccountCredentials.from_json_keyfile_name('client_secret.json', scope)
 client = gspread.authorize(creds)
  
 # Find a workbook by name and open the first sheet
-# Make sure you use the right name here.
 sheet = client.open(sheet_name).sheet1
  
 # Extract and print all of the values
